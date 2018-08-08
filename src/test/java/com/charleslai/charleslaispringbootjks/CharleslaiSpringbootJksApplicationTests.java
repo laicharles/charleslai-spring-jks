9a25f9c4779a01c9383b391c3846c680fdb61b83
charleslai@139.com <charleslai@139.com>
2018-08-08 14:33:36 +0800

    创建 charleslai-spring-jks
    在排除 springboot tomcat  采用 undertow 进行 无阻塞 应用
    
    进行 ssl 验证 进行操作

---
+++ src/test/java/com/charleslai/charleslaispringbootjks/CharleslaiSpringbootJksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.charleslai.charleslaispringbootjks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CharleslaiSpringbootJksApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
